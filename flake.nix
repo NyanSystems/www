d27e61d39199b6f887fb5bc7874f5ab5d555606b
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-12-01 11:58:59 +0500

    generated nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NyanSystems website and its web infra";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } @inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ 
+            pkgs.llvmPackages.llvm
+            pkgs.llvmPackages.clang
+            pkgs.cmake
+            # Add any other dependencies your project needs
+          ];
+        };
+      }
+    );
+}
